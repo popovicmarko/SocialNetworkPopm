c57e3788272a8d9229a42b34091802ab5410943a
popovicmarko <popovicmarko350@gmail.com>
2023-05-10 18:12:43 +0200

    complete opening and closing of the registration card

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.querySelector('#registracija').addEventListener('click', () => {
+    document.querySelector('.custom-model').style.display = 'block';
+})
+document.querySelector('#closeModal').addEventListener('click', () => {
+    document.querySelector('.custom-model').style.display = 'none';
+})
