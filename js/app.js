f18352ba7c125747b9d42179bb102258908eab95
popovicmarko <popovicmarko350@gmail.com>
2023-05-11 13:02:37 +0200

    completed logic for register button and x button

--- js/app.js
+++ js/app.js
@@ -4,3 +4,7 @@ document.querySelector('#registracija').addEventListener('click', () => {
 document.querySelector('#closeModal').addEventListener('click', () => {
     document.querySelector('.custom-model').style.display = 'none';
 })
+ let a = document.querySelector('#closeModal')
+ a.addEventListener('click', e => {
+    e.preventDefault();
+ })
