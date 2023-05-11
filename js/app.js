aba1518baf920621e964eb0be3d0d551e21a8fe4
popovicmarko <popovicmarko350@gmail.com>
2023-05-11 14:01:41 +0200

    validation form finish

--- js/app.js
+++ js/app.js
@@ -8,3 +8,40 @@ document.querySelector('#closeModal').addEventListener('click', () => {
  a.addEventListener('click', e => {
     e.preventDefault();
  })
+
+ let config = {
+    'korisnicko_ime':{
+        request: true,
+        email: true,
+        minLength: 5,
+        maxLength: 50,
+    },
+    'register_email':{
+        request: true,
+        email: true,
+        minLength: 5,
+        maxLength: 50,
+    },
+    'register_lozinka':{
+        request: true,
+        email: true,
+        minLength: 7,
+        maxLength: 25,
+        matching: 'ponovi_lozinku'
+    },
+    'ponovi_lozinku':{
+        request: true,
+        email: true,
+        minLength: 7,
+        maxLength: 25,
+        matching: 'register_lozinka'
+    }
+ };
+ const validator = new Validator(config, "#registrationForm");
+ document.querySelector('#registrationForm').addEventListener('submit', e =>{
+    e.priventDefault();
+    if(validator.validationPassed()){
+        alert('Sve je ok')
+    } else
+    alert('nije ok')
+ });
